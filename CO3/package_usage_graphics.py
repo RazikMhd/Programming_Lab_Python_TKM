542a3dbf7b61ed26cc7552646c0b5fa5d65a71b1
Razik <rasikmhd10@gmail.com>
2021-03-17 19:55:28 +0530

    Sphere added

--- CO3/package_usage_graphics.py
+++ CO3/package_usage_graphics.py
@@ -1,6 +1,7 @@
 
 from CO3.graphics.circle import *
 import CO3.graphics.rectangle
+import CO3.graphics.graphics3D.sphere
 from CO3.graphics.graphics3D import cuboid
 
 l =20
@@ -9,6 +10,7 @@ h =35
 
 # regular import
 print("Area of Rectangle : "+str(CO3.graphics.rectangle.getPerimeter(l,b)))
+print("Area of Sphere : "+str(CO3.graphics.graphics3D.sphere.getArea(l)))
 
 # shortened import
 print("Area of Cuboid : "+str(cuboid.getArea(l,b,h)))
